52ad1216cc19f629fa66ec51e74be4a1213f4e5d
Vijeth Kalavarkar <vkalavarkar321@gmail.com>
2025-02-19 12:29:27 +0530

    Added price updates

--- src/App.js
+++ src/App.js
@@ -22,21 +22,21 @@ function App() {
             <img src="placeholder.jpg" alt="Product 1" />
             <h2>Product 1</h2>
             <p>Description of product 1.  Add some details here.</p>
-            <p class="price">$29.99</p>
+            <p class="price">$20.00</p>
         </div>
 
         <div class="product">
             <img src="placeholder.jpg" alt="Product 2" />
             <h2>Product 2</h2>
             <p>Description of product 2. Add some details here.</p>
-            <p class="price">$49.99</p>
+            <p class="price">$40.00</p>
         </div>
 
         <div class="product">
             <img src="placeholder.jpg" alt="Product 3" />
             <h2>Product 3</h2>
             <p>Description of product 3. Add some details here.</p>
-            <p class="price">$19.99</p>
+            <p class="price">$60.00</p>
         </div>
 
         </main>
