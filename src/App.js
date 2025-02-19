d6b273da42cc1ccaa58422dbd36aa149a9e999d4
Vijeth Kalavarkar <vkalavarkar321@gmail.com>
2025-02-19 12:36:15 +0530

    Added some more product field

--- src/App.js
+++ src/App.js
@@ -39,6 +39,27 @@ function App() {
             <p class="price">$60.00</p>
         </div>
 
+        <div class="product">
+            <img src="placeholder.jpg" alt="Product 1" />
+            <h2>Product 1</h2>
+            <p>Description of product 1.  Add some details here.</p>
+            <p class="price">$20.00</p>
+        </div>
+
+        <div class="product">
+            <img src="placeholder.jpg" alt="Product 2" />
+            <h2>Product 2</h2>
+            <p>Description of product 2. Add some details here.</p>
+            <p class="price">$40.00</p>
+        </div>
+
+        <div class="product">
+            <img src="placeholder.jpg" alt="Product 3" />
+            <h2>Product 3</h2>
+            <p>Description of product 3. Add some details here.</p>
+            <p class="price">$60.00</p>
+        </div>
+
         </main>
 
     <footer>
